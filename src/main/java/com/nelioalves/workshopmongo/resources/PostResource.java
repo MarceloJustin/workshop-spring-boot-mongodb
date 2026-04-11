d2e775588e979f80fa28c358e4c0f6530d1eb64c
Marcelo Justin <marcelojustin3@gmail.com>
2026-04-10 22:16:37 -0300

    Consulta com varios Critérios

--- src/main/java/com/nelioalves/workshopmongo/resources/PostResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.nelioalves.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.nelioalves.workshopmongo.domain.Post;
 import com.nelioalves.workshopmongo.resources.util.URL;
 import com.nelioalves.workshopmongo.services.PostService;
@@ -30,6 +32,18 @@ public class PostResource {
     public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="")String text) {
 	text = URL.decodeParam(text);
 	List<Post> list = service.findByTitle(text);
+	return ResponseEntity.ok().body(list);
+    }
+    
+    @GetMapping("/fullsearch")
+    public ResponseEntity<List<Post>> fullSearch( 
+	    @RequestParam(value="text", defaultValue="")String text,
+    	    @RequestParam(value="minDate", defaultValue="")String minDate,
+    	    @RequestParam(value="MaxDate", defaultValue="")String maxDate) {
+	text = URL.decodeParam(text);
+	Date min = URL.convertDate(minDate, new Date(0L));
+	Date max = URL.convertDate(maxDate, new Date());
+	List<Post> list = service.fullSearch(text, min, max);
 	return ResponseEntity.ok().body(list);
     }
 }
